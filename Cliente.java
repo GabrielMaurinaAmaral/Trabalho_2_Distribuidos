4e2dd4633bc5fe45be161ccfa12dc158196e4195
Gabriel Maurina Amaral <gabrielmaurina@alunos.utfpr.edu.br>
2025-05-21 15:28:48 -0300

    ajustando cliente e servidor

--- Cliente.java
+++ Cliente.java
@@ -11,68 +11,68 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Cliente implements Runnable{
+public class Cliente implements Runnable {
 
 	private int porta;
 	private String host;
-	private String nome_cliente;
+	private String nomeUsuario;
 
 	public Cliente(String host, int porta) {
 		this.host = host;
 		this.porta = porta;
 	}
 
-	public Cliente(String host, int porta, String nome_cliente) {
+	public Cliente(String host, int porta, String nomeUsuario) {
 		this.host = host;
 		this.porta = porta;
-		this.nome_cliente = nome_cliente;
+		this.nomeUsuario = nomeUsuario;
 	}
 
-	public void run(){
+	public void run() {
 
 		try {
-			//Dados dos servidor
 			InetAddress endereco;
 			endereco = InetAddress.getByName(host);
 
-			//Passo 2: Conectar o socket a porta do servidor
 			Socket socket = new Socket(endereco, porta);
 
-			//Passo 3: Cria canais de comunicação (streams) de entrada e saída
 			DataInputStream in = new DataInputStream(socket.getInputStream());
 			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-			
-			//Monta mensagem 
-			String resposta = "Olá servidor eu sou " + socket.getInetAddress().getHostAddress() + ":" + socket.getPort(); 
 
-			out.writeUTF(resposta); // Envia a mensagem
+			String resposta = "Olá servidor eu sou " + socket.getInetAddress().getHostAddress() + ":"
+					+ socket.getPort();
+			out.writeUTF(resposta);
 
-			String mensagem = in.readUTF(); //Bloqueia até que uma mensagem seja recebida
+			String mensagem = in.readUTF();
+			System.out.println("[Cliente] Mensagem recebida de " + socket.getInetAddress().getHostAddress() + ":"
+					+ socket.getPort() + "->" + mensagem);
 
-			// Processa a mensagem
-			System.out.println("[Cliente] Mensagem recebida de " +  socket.getInetAddress().getHostAddress() + ":" + socket.getPort() + "->" + mensagem);
-
-			//Cria um canal de comunicação para transimitir um objeto ao servidor
 			ObjectOutputStream outObject = new ObjectOutputStream(socket.getOutputStream());
-		
-			outObject.writeObject(p); // Envia a mensagem contendo o objeto
-			
-			// Passo 4 : Fechar o socket
-			socket.close();
+
+			while (true) {
+				Scanner scanner = new Scanner(System.in);
+				System.out.print("[Cliente] Digite o número de clientes: ");
+				String conteudo = scanner.nextLine();
+				scanner.close();
 				
-		}catch(IOException e) {
+				Mensagem m = new Mensagem(host, socket.getInetAddress().getHostAddress(), conteudo);
+				outObject.writeObject(m);
+			}
+
+			socket.close();
+
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("[Cliente] Digite o número de clientes: ");
+		System.out.print("[Cliente] Digite o nome de clientes: ");
 		String nome = scanner.nextLine();
 		scanner.close();
-			
-		Thread cliente = new Thread(new Cliente("localhost", 1234, nome));			
+
+		Thread cliente = new Thread(new Cliente("localhost", 1234, nome));
 		cliente.start();
 	}
 }
--- Servidor.java
+++ Servidor.java
@@ -6,7 +6,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
 
-public class Servidor{
+public class Servidor {
 
 	private int porta;
 	private ServerSocket conexao;
@@ -16,95 +16,74 @@ public class Servidor{
 		this.porta = porta;
 	}
 
-	public void run() throws IOException{
-
-		// Passo 1 : criar socket de conexão
+	public void run() throws IOException {
 		conexao = new ServerSocket(porta);
-		//Informa a porta onde o servidor vai “ouvir” conexões
-		//O socket ainda não está conectado a ninguém.
-		
+
 		try {
-			while(true) {
+			while (true) {
 				System.out.println("[Servidor] Aguardando conexões na porta " + conexao.getLocalPort() + "...");
 
-				conexao.setSoTimeout(30000); // 30 segundos de inatividade para leitura
-		
-				//Passo 2: aguarda conexão do cliente
-				socket = conexao.accept(); 
-				//- O método accept() bloqueia o programa até que um cliente tente se conectar.
-				//- Quando alguém se conecta, retorna um Socket específico para aquele cliente.
-				//- Esse Socket é usado para se comunicar com aquele cliente individualmente.
-		
-				
+				conexao.setSoTimeout(20000);
+				socket = conexao.accept();
 				System.out.println("[Servidor] Conexão aceita ..." + socket.getLocalPort());
-				
+
 				Thread cliente = new Thread(new Processador(socket));
 				cliente.start();
-			
 			}
-		}catch(SocketTimeoutException e) {
-			//Passo 6: fecha o socket de conexão
+		} catch (SocketTimeoutException e) {
 			System.out.println("[Servidor] Conexão encerrada por inatividade.");
 			conexao.close();
-		}		
+		}
 	}
-	
-	class Processador implements Runnable{
+
+	class Processador implements Runnable {
 		private Socket socket;
 		DataInputStream in;
 		DataOutputStream out;
-		
+
 		public Processador(Socket socket) {
 			this.socket = socket;
 		}
-		
+
 		@Override
 		public void run() {
 			try {
-				
-				socket.setSoTimeout(20000); // 20 segundos de inatividade para leitura
-				
-				//Passo 3: Cria canais de comunicação (streams) de entrada e saída com o cliente
+				socket.setSoTimeout(20000);
+
 				DataInputStream in = new DataInputStream(socket.getInputStream());
 				DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-				
-				//Passo 4: Realiza comunicação
-				String mensagem = in.readUTF(); // Lê a mensagem (bloqueia até que uma mensagem seja recebida)
-				
-				//Processa a mensagem recebida do cliente
-				System.out.println("[Servidor] Mensagem recebida de " +  socket.getInetAddress().getHostAddress() + ":" + socket.getPort() + "->" + mensagem);
-				
-				//Constroi a mensagem de resposta
-				String resposta = "Olá cliente " + socket.getInetAddress().getHostAddress() + ":" + socket.getPort(); 
-				
-				out.writeUTF(resposta); // Envia a mensagem ao cliente
-				
-				//Cria um canal de comunicação para receber objetos
+
+				String mensagem = in.readUTF();
+				System.out.println("[Servidor] Mensagem recebida de " + socket.getInetAddress().getHostAddress() + ":"
+						+ socket.getPort() + "->" + mensagem);
+
+				String resposta = "Olá cliente " + socket.getInetAddress().getHostAddress() + ":" + socket.getPort();
+				out.writeUTF(resposta);
+
 				ObjectInputStream inObject = new ObjectInputStream(socket.getInputStream());
-				
-				Pessoa p = (Pessoa) inObject.readObject(); // Lê o objeto contido na mensagem (bloqueia até que uma mensagem seja recebida)
-				
-				System.out.println("[Servidor] Recebi a pessoa " + p.toString() + " de " +  socket.getInetAddress().getHostAddress() + ":" + socket.getPort());
-				
-				//Passo 5: fecha o socket com o cliente
+
+				while (true) {
+					Mensagem m = (Mensagem) inObject.readObject();
+					System.out.println("[Servidor] Recebi a pessoa " + p.toString() + " de "
+							+ socket.getInetAddress().getHostAddress() + ":" + socket.getPort());
+				}
+
 				socket.close();
-			
-			}catch(SocketTimeoutException e) {
-				System.out.println("[Servidor] Conexão com o cliente "+socket.getInetAddress().getHostAddress() +":"+ socket.getPort()  +" encerrada por inatividade.");
-			    try {
+
+			} catch (SocketTimeoutException e) {
+				System.out.println("[Servidor] Conexão com o cliente " + socket.getInetAddress().getHostAddress() + ":"
+						+ socket.getPort() + " encerrada por inatividade.");
+				try {
 					socket.close();
 				} catch (IOException e1) {
-					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
-			}catch(IOException e) {
+			} catch (IOException e) {
 				e.printStackTrace();
 			} catch (ClassNotFoundException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
-			} 
+			}
 		}
-		
 	}
 
 	public static void main(String[] args) {
@@ -118,5 +97,4 @@ public class Servidor{
 			e.printStackTrace();
 		}
 	}
-
 }
